b840d6395990d6783c73b40cb51af7cd66d45b6e
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-03-05 15:10:23 +1100

    getinstallinfo() added but not checked

---
+++ R/getinstallinfo.R
@@ -0,0 +1,17 @@
+#' Get install information about a package
+#' @description Quickly get LibPath, version, built and package date of a package
+#' @return A data frame
+#' @param name A package name
+#' @export
+installinfo <- function(name){
+info <- utils::packageDescription(name)
+pkgimportant <- paste(scorecompdirinfo[c("Suggests", "Imports", "Depends")], collapse = ", ")
+pkgimportant <- unlist(strsplit(gsub("[^,ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz]", "", pkgimportant), ","))
+pkgimportant <- c(pkgimportant, name)
+pkginfo <- as.data.frame(utils::installed.packages(fields = c("Built", "Packaged")))
+pkginfo <- pkginfo[pkginfo$Package %in% pkgimportant, 
+                   c("Package", "LibPath", "Version",
+                     "Built", "Packaged")]
+return(pkginfo)
+}
+
