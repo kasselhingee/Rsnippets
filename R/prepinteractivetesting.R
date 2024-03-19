37809947d4dd49ee37f1690b0dd752e0665f53e1
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-03-20 08:25:08 +1100

    help for prepinteractivetesting mentions that maybe the difference is the presence of NDEBUG flag

--- R/prepinteractivetesting.R
+++ R/prepinteractivetesting.R
@@ -1,5 +1,8 @@
-#' Short code to prepare environment for running content of testthat tests interactively
-#' @description I keep getting bugs using C++ and RStudio/devtools built in methods for testing. Something how things compile and not finding C++ objects. This little function seems to get around the problem largely by installing the package differently, if at all.
+#' Prepare for running content of testthat tests interactively
+#' @description 
+#' This installs a local package using `devtools::install()` then attaches the namespace of the package and `testthat`.
+#' I'm not sure why this led to different behaviour than `devtools::test()` and `devtools::load_all()`.
+#' I think `devtools::test()` may compile C++ without the `NDEBUG` flag so that any assertions in the C++ code get thrown and cause R to abort.
 #' @details Uses `attach()`. Perhaps I could use something like `devtools::load_all(compile = TRUE)`
 #' @param path Directory of package. If this fails to get a package name, the it will try using path as the package name.
 #' @export
